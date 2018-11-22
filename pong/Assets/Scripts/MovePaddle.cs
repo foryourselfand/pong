f6a87bd4262de8bcdf96ce70c19045fbad312f08
foryourselfand <foryourselfand@gmail.com>
2018-11-22 18:13:44 +0300

    Moving Paddle Script

---
+++ pong/Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour
+{
+    public float speed = 30f;
+
+    private void FixedUpdate()
+    {
+        var verticalPress = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, verticalPress) * speed;
+    }
+}
